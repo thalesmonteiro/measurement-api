49b556bc7b7cae0bfbd5139107a7b0411e47b211
Thales Monteiro <thalesmonteir@gmail.com>
2020-10-12 15:52:15 -0300

    first commit

---
+++ pkg/controllers/usercontroller.go
@@ -0,0 +1,12 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	//"github.com/example/simple-REST/pkg/utils"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+	//"github.com/example/simple-REST/pkg/models"
+)
+
